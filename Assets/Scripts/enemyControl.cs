8d87bc29e42748f0424d7e40e0d82d5b1f4eeb37
jaine <essi.jukkala@gmail.com>
2017-03-07 20:53:29 +0200

    enemies move towards player

--- Assets/Scripts/enemyControl.cs
+++ Assets/Scripts/enemyControl.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class enemyControl : MonoBehaviour {
-	//float speed = 2f;		
-	//bool dead = false;	
 	public GameObject player;
 	public GameObject explosion;
 	public GameObject reloadTextObject;
 
 	public static bool playerExists = true;
+
+	float moveForceX = 2f;
+    float moveForceY = 1f;
 
 	void Start(){
 		player = GameObject.FindWithTag("Player");
@@ -42,7 +43,22 @@ public class enemyControl : MonoBehaviour {
 		Physics2D.IgnoreLayerCollision (8, 8, true);
 
 		if(playerExists){
-
+			float dist = Vector3.Distance(player.transform.position, gameObject.transform.position);
+			//move towards player if somewhat close, but do not move to touching
+			if(dist < 4){
+				if(Mathf.Abs(player.transform.position.x-gameObject.transform.position.x) > 0.5f){
+					if(player.transform.position.x < gameObject.transform.position.x)
+						GetComponent<Rigidbody2D>().AddForce (Vector2.left * moveForceX);
+					else
+						GetComponent<Rigidbody2D>().AddForce (Vector2.right * moveForceX);
+				}
+				else if(Mathf.Abs(player.transform.position.y-gameObject.transform.position.y) > 0.5f){
+					if(player.transform.position.y < gameObject.transform.position.y)
+						GetComponent<Rigidbody2D>().AddForce (Vector2.down * moveForceY);
+					else
+						GetComponent<Rigidbody2D>().AddForce (Vector2.up * moveForceY);
+				}
+			}
 
 		}
 		else{
@@ -52,7 +68,7 @@ public class enemyControl : MonoBehaviour {
 
 	IEnumerator Reload(){
 		reloadTextObject.GetComponent<Text>().enabled = true;
-		yield return new WaitForSeconds(5);
+		yield return new WaitForSeconds(3);
 		playerExists = true;
 		spawnEnemies.enemyCount=0;
 		Scene scene = SceneManager.GetActiveScene();
