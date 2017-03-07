6b4b268eac6029e0ec9bb8d1fd7f3b8ec2388237
jaine <essi.jukkala@gmail.com>
2017-03-07 21:39:29 +0200

    enemies now shoot the player

---
+++ Assets/Scripts/ballControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballControl : MonoBehaviour {
+	public GameObject player;
+	public GameObject explosion;
+
+	void Start(){
+		player = GameObject.FindWithTag("Player");
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Collider2D collider = col.collider;
+
+		if(collider.tag == "Player"){
+			GameObject exp = Instantiate(explosion, transform.position, Quaternion.identity);
+			Destroy(collider.gameObject);
+			Destroy(gameObject);
+			Destroy(exp, 2);
+			enemyControl.playerExists = false;
+		}
+		else if (collider.tag == "Laser") { 
+			Destroy(gameObject);
+			Destroy(collider.gameObject);
+		}
+	}
+}
--- Assets/Scripts/enemyControl.cs
+++ Assets/Scripts/enemyControl.cs
@@ -7,9 +7,13 @@ using UnityEngine.UI;
 public class enemyControl : MonoBehaviour {
 	public GameObject player;
 	public GameObject explosion;
+	public GameObject ball;
 	public GameObject reloadTextObject;
 
 	public static bool playerExists = true;
+
+	float shootTime = 6f;
+	bool shot = false;
 
 	float moveForceX = 2f;
     float moveForceY = 1f;
@@ -25,8 +29,10 @@ public class enemyControl : MonoBehaviour {
 		Collider2D collider = col.collider;
 
 		if (collider.tag == "Laser") { 
+			GameObject exp = Instantiate(explosion, transform.position, Quaternion.identity);
 			Destroy(gameObject);
 			Destroy(collider.gameObject);
+			Destroy(exp, 2);
 			spawnEnemies.enemyCount--;
 		}
 		else if(collider.tag == "Player"){
@@ -43,20 +49,31 @@ public class enemyControl : MonoBehaviour {
 		Physics2D.IgnoreLayerCollision (8, 8, true);
 
 		if(playerExists){
-			float dist = Vector3.Distance(player.transform.position, gameObject.transform.position);
+			float dist = Vector3.Distance(player.transform.position, transform.position);
 			//move towards player if somewhat close, but do not move to touching
 			if(dist < 4){
-				if(Mathf.Abs(player.transform.position.x-gameObject.transform.position.x) > 0.5f){
-					if(player.transform.position.x < gameObject.transform.position.x)
+				if(Mathf.Abs(player.transform.position.x-transform.position.x) > 0.5f){
+					if(player.transform.position.x < transform.position.x)
 						GetComponent<Rigidbody2D>().AddForce (Vector2.left * moveForceX);
 					else
 						GetComponent<Rigidbody2D>().AddForce (Vector2.right * moveForceX);
 				}
-				else if(Mathf.Abs(player.transform.position.y-gameObject.transform.position.y) > 0.5f){
-					if(player.transform.position.y < gameObject.transform.position.y)
+				else if(Mathf.Abs(player.transform.position.y-transform.position.y) > 0.5f){
+					if(player.transform.position.y < transform.position.y)
 						GetComponent<Rigidbody2D>().AddForce (Vector2.down * moveForceY);
 					else
 						GetComponent<Rigidbody2D>().AddForce (Vector2.up * moveForceY);
+				}
+				if(shot && shootTime > 0){
+					shootTime -= Time.deltaTime;
+				}
+				else{
+					shot = false;
+					shootTime = 6f;
+				}
+				if(!shot){
+					shot = true;
+					Shoot();
 				}
 			}
 
@@ -73,5 +90,18 @@ public class enemyControl : MonoBehaviour {
 		spawnEnemies.enemyCount=0;
 		Scene scene = SceneManager.GetActiveScene();
 		SceneManager.LoadScene(scene.name);
+	}
+
+	void Shoot(){
+		GameObject shot = Instantiate(ball, transform.position, Quaternion.identity);
+		int dir = 1;
+		if(player.transform.position.x < transform.position.x)
+			dir = -1;
+		if(player.transform.position.y > transform.position.y)
+			shot.GetComponent<Rigidbody2D>().velocity = new Vector2 (dir*0.2f, 0.5f);
+		else
+			shot.GetComponent<Rigidbody2D>().velocity = new Vector2 (dir*0.2f, -0.5f);
+
+		Destroy(shot, 2f);
 	}
 }
